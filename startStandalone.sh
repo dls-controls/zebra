0b82e14697e4a4f1cb88665f4b6debd937879aad
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2014-04-23 08:06:29 +0000

    zebra: added link to new manual

--- startStandalone.sh
+++ startStandalone.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 HERE="$(dirname "$0")"
-# This is the PV prefix, change this if you have more than one zebra on the network
-PREFIX="MYZEBRA"
+# Take the PV prefix from the first argument from the script if supplied
+# This must be specified if there are more than one instances of the IOC on
+# the network
+# Default gives PVs that look like MYZEBRA:AND1_INV
+PREFIX="${1:-TESTZEBRA}"
+# By default /dev/ttyUSB0 is not readable or writeable, make it so
+[ -e /dev/ttyUSB0 ] || {
+    echo /dev/ttyUSB0 does not exist, please plug in Zebra via a USB-RS232 converter;
+    exit 1
+}
+[ -w /dev/ttyUSB0 ] || sudo chmod a+rw /dev/ttyUSB0
+# Start the IOC
 gnome-terminal -e "${HERE}/iocs/test/bin/linux-x86_64/sttest.sh ${PREFIX}" &
+# Start the Gui
 "${HERE}/zebraGui/zebraGui" "${PREFIX}" &
