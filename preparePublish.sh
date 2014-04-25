c51a37108f05dfecd3feec247b0bc196ab6c244f
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2014-04-25 10:52:01 +0000

    zebra: added preparePublish to pull in zebra gui

--- preparePublish.sh
+++ preparePublish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 make -C iocs/test
 rm -rf zebraGui
-cp -rf /dls_sw/work/tools/RHEL6-x86_64/zebraGui/prefix zebraGui
-cp -rf /dls_sw/work/tools/RHEL6-x86_64/qt_static/prefix/qml/* zebraGui/
-cp -rf /dls_sw/work/tools/RHEL6-x86_64/zebraGui/zebraGui zebraGui/src
+cp -rf /dls_sw/prod/tools/RHEL6-x86_64/zebraGui/1-0/prefix zebraGui
+cp -rf /dls_sw/prod/tools/RHEL6-x86_64/zebraGui/1-0/zebraGui zebraGui/src
 
