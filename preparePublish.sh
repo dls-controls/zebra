ffc5d3cf41812eb8b88b9a8b7b8f9ec93955bb5a
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2014-04-25 14:07:23 +0000

    zebra: make sure we export CA_MAX_ARRAY_BYTES in standalone script

--- preparePublish.sh
+++ preparePublish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 make -C iocs/test
 rm -rf zebraGui
-cp -rf /dls_sw/prod/tools/RHEL6-x86_64/zebraGui/1-0/prefix zebraGui
-cp -rf /dls_sw/prod/tools/RHEL6-x86_64/zebraGui/1-0/zebraGui zebraGui/src
+#ZEBRAGUI=/dls_sw/work/tools/RHEL6-x86_64/zebraGui
+ZEBRAGUI=/dls_sw/prod/tools/RHEL6-x86_64/zebraGui/1-0/
+cp -rf $ZEBRAGUI/prefix zebraGui
+cp -rf $ZEBRAGUI/zebraGui zebraGui/src
 
