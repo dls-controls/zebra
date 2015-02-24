d9786398fb73ce1a5d6bc2784bc278f486795017
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2015-02-24 16:09:06 +0000

    zebra: ship the right gui...

--- preparePublish.sh
+++ preparePublish.sh
@@ -2,7 +2,7 @@
 make -C iocs/test
 rm -rf zebraGui
 #ZEBRAGUI=/dls_sw/work/tools/RHEL6-x86_64/zebraGui
-ZEBRAGUI=/dls_sw/prod/tools/RHEL6-x86_64/zebraGui/1-1/
+ZEBRAGUI=/dls_sw/prod/tools/RHEL6-x86_64/zebraGui/1-2/
 cp -rf $ZEBRAGUI/prefix zebraGui
 cp -rf $ZEBRAGUI/zebraGui zebraGui/src
 
