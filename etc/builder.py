c454f446ec9aaab0244d16d9c6c3caf1e6020288
Sky French <sky.french@diamond.ac.uk>
2018-08-14 12:20:52 +0100

    updating areaDetector dependency to ADCore (part of RHEL7 migration)

--- etc/builder.py
+++ etc/builder.py
@@ -1,6 +1,6 @@
 from iocbuilder import AutoSubstitution, Device
 from iocbuilder.modules.asyn import Asyn, AsynPort, _AsynOctetInterface
-from iocbuilder.modules.areaDetector import AreaDetector, _ADBase
+from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate
 from iocbuilder.modules.busy import Busy
 from iocbuilder.modules.motor import MotorLib
 from iocbuilder.arginfo import *
@@ -11,13 +11,14 @@ class _zebraTemplate(AutoSubstitution):
     ## Parse this template file for macros
     TemplateFile = 'zebra.template'
 
-class zebra(_ADBase):
+class zebra(AsynPort):
     DbdFileList = ["zebraSupport"]
     LibFileList = ["zebra"]
     _SpecificTemplate = _zebraTemplate
-    Dependencies = (Asyn, AreaDetector, MotorLib, Busy)
-    def __init__(self, serialPort, MAXBUF=5, MAXMEM=0, **args):
-        self.__super.__init__(R=args["Q"] + ":", **args)
+    Dependencies = (Asyn, ADCore, MotorLib, Busy)
+    def __init__(self, PORT, serialPort, NELM=100000, MAXBUF=5, MAXMEM=0, **args):
+        self.__super.__init__(PORT)
+        self.__dict__.update(locals())
         self.serialPort = serialPort
         self.MAXBUF = MAXBUF
         self.MAXMEM = MAXMEM
@@ -26,12 +27,14 @@ class zebra(_ADBase):
         print '#zebraConfig(Port, SerialPort, MaxPosCompPoints, MaxBuffers, MaxMemory)'
         print 'zebraConfig("%(PORT)s", "%(serialPort)s", %(NELM)s, %(MAXBUF)d, %(MAXMEM)d)' % self.__dict__
 
-    ArgInfo = _ADBase.ArgInfo.filtered(without = ["R"]) + \
+    ArgInfo = ADBaseTemplate.ArgInfo.filtered(without = ["R"]) + \
               _zebraTemplate.ArgInfo.filtered(without=["EMPTY"]) + \
               makeArgInfo(__init__,
+                          PORT = Simple("asyn Port Name", str),
                           serialPort = Ident ("Serial port name", _AsynOctetInterface),
-                          MAXBUF = Simple("Maximum number of buffers (areaDetector)", int),
-                          MAXMEM = Simple("Maximum memory (areaDetector)", int))
+                          NELM = Simple("Max points for position compare results", int),
+                          MAXBUF = Simple("Maximum number of buffers ", int),
+                          MAXMEM = Simple("Maximum memory ", int))
 
 class zebraLastDivDiff(AutoSubstitution):
     '''Makes a record pointing to DIV$(DIV) that will display the difference
